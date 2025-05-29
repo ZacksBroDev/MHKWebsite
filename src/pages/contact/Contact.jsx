7d919c4c80af8028dd9cd13c328995b2788c028e
Zackary Brown <zackaryzbrown@gmail.com>
2025-05-28 22:38:31 -0600

    added basic contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import './contact.css';
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-      <p>This is the contact page of our application.</p>
-    </div>
+    <>
+      <h1>Contact Form</h1>
+      <div className="contact-form">
+        <div><input className="input1" type="text" placeholder="Your Name"/>
+      <input className="input2" type="email" placeholder="Your Email"/></div>
+        <div><input className="input3" type="phone" placeholder="Your Phone"/>
+        <input className="input4" type="text" placeholder="Subject"/></div>
+        <div><textarea className="input5" placeholder="Your Message"></textarea></div>
+      </div>
+      <button className="submit-button" type="submit">SUBMIT</button>
+    </>
   );
 }
 export default Contact;
