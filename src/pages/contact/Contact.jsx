0b337d1b740144f42dab3f7a2a9afc5620795bb9
Zackary Brown <zackaryzbrown@gmail.com>
2025-05-30 16:43:32 -0600

    added Working Contact form

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,43 +1,49 @@
 import React from "react";
 import './contact.css';
+import useWeb3Forms from '@web3forms/react';
+
 
 const Contact = () => {
-  const [result, setResult] = React.useState("");
+  const accessKey = 'bd3b1db0-a150-4eb9-8c61-27249b4f0d98';
+  const { submit } = useWeb3Forms({
+    access_key: accessKey,
+    settings: {
+      from_name: 'Mile High Karate',
+      subject: 'New Contact Message from your Website',
+    },
+    onSuccess: (message, data) => {
+      console.log('Success', message, data);
+    },
+  });
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
-    setResult("Sending....");
-    const formData = new FormData(event.target);
-
-    formData.append("access_key", "bd3b1db0-a150-4eb9-8c61-27249b4f0d98");
-
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    submit({
+      name: e.target.name.value,
+      email: e.target.email.value,
+      phone: e.target.phone.value,
+      subject: e.target.phone.value,
+      message: e.target.message.value,
     });
-
-    const data = await response.json();
-
-    if (data.success) {
-      setResult("Form Submitted Successfully");
-      event.target.reset();
-    } else {
-      console.log("Error", data);
-      setResult(data.message);
-    }
+    e.target.reset();
   };
+
   return (
     <>
       <h1>Contact Form</h1>
-      <form  onSubmit={onSubmit} className="contact-form">
-        <div><input id="input1" type="text" placeholder="Your Name"/></div>
-        <div><input id="input2" type="email" placeholder="Your Email"/></div>
-        <div><input id="input3" type="phone" placeholder="Your Phone"/></div>
-        <div><input id="input4" type="text" placeholder="Subject"/></div>
-        <div><textarea id="input5" placeholder="Your Message"></textarea></div>
-      </form>
-      <button className="submit-button" type="submit">SUBMIT</button>
+      <div>
+        <form  onSubmit={handleSubmit} className="contact-form">
+          <div><input name="name" id="input1" type="text" placeholder="Your Name" required/></div>
+          <div><input name="email" id="input2" type="email" placeholder="Your Email" required/></div>
+          <div><input name="phone" id="input3" type="phone" placeholder="Your Phone" required/></div>
+          <div><input name="subject" id="input4" type="text" placeholder="Subject" required/></div>
+          <div><textarea name="message" id="input5" placeholder="Your Message" required></textarea></div>
+          <img src="/src/assets/img/MHK-family.png"/>
+          <button className="submit-button" type="submit">SUBMIT</button>
+        </form>
+       
+      </div>
     </>
   );
-}
+};
 export default Contact;
