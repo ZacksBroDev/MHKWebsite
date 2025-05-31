45df7a5d2c73c90f7b8f3fa2fe72269f2b8198ba
Zackary Brown <zackaryzbrown@gmail.com>
2025-05-31 00:31:22 -0600

    start login page

--- src/pages/Login/logIn.jsx
+++ src/pages/Login/logIn.jsx
@@ -1,22 +1,17 @@
 import React from "react";
+import './logIn.css';
 
 function Login() {
   return (
-    <div className="logIn">
-      <h2 className="SIL">Sign in</h2>
-      <h4 className="cred-label">Enter your credentials to access your account</h4>
-      <div>
-          <div><label className="userLabel">User Name</label></div>
-          <input className="userIn" type="text" placeholder="UserName" />
-      </div>
-      <div>
-        <div><label className="userLabel">Password</label></div>
-      <input className="pwIn" type="password" placeholder="Password" />
-      </div>
-      <div className="userLabel"><input type="checkbox" /><label>Remember Me</label></div>
-      <div className="SUB"><button>Sign up</button></div>
-      <a to="/home"/><button>home</button>
-    </div>
+    <form action="some" className="login-form">
+      <h2 className="cred-label">Enter your credentials to access your account</h2>
+        <input className="userIn" type="text" placeholder="UserName"/>
+      <input className="pwIn" type="password" placeholder="Password"/>
+      <input type="password" placeholder="Desk Password" />
+      <button id="signUp" className="submit-button">SIGN UP</button>
+      <button id="logIn" className="submit-button">LOG IN</button>
+    </form>
+      
   );
 }
 export default Login;
