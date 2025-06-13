04f0a890a3e81fd4ffae699c985e2d9d6f38dea1
ZacksBroDev <zackaryzbrown@gmail.com>
2025-06-13 14:37:04 -0400

    added pages

---
+++ src/pages/schedule/Schedule.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Schedule = () => {
+  return (
+    <>
+      <h1>Schedule</h1>
+      <p>Details about the schedule will go here.</p>
+    </>
+  );
+};
+export default Schedule;
