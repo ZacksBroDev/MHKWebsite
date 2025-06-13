069bc6b2c81ac4424db2f29d22ae6b0a087bd76b
ZacksBroDev <zackaryzbrown@gmail.com>
2025-06-12 23:37:50 -0600

    added div

--- src/pages/Login/logIn.jsx
+++ src/pages/Login/logIn.jsx
@@ -3,15 +3,17 @@ import './logIn.css';
 
 function Login() {
   return (
-    <form action="some" className="login-form">
-      <h2 className="cred-label">Enter your credentials to access your account</h2>
+    <>
+      <h1>Login</h1>
+      <form action="some" className="login-form">
+        <h2 className="cred-label">Enter your credentials to access your account</h2>
         <input className="userIn" type="text" placeholder="UserName"/>
-      <input className="pwIn" type="password" placeholder="Password"/>
-      <input type="password" placeholder="Desk Password" />
-      <button id="signUp" className="submit-button">SIGN UP</button>
-      <button id="logIn" className="submit-button">LOG IN</button>
-    </form>
-      
+        <input className="pwIn" type="password" placeholder="Password"/>
+        <input type="password" placeholder="Desk Password" />
+        <button id="signUp" className="submit-button">SIGN UP</button>
+        <button id="logIn" className="submit-button">LOG IN</button>
+      </form>
+    </>
   );
 }
 export default Login;
