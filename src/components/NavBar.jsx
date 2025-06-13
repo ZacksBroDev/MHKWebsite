44e852e3c568d18da0dd94fc8be098fcfde6a631
ZacksBroDev <zackaryzbrown@gmail.com>
2025-06-13 14:15:43 -0400

    make chnages to main css and routes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,13 +8,12 @@ const NavBar = () => {
         <nav>
           <ul className='nav-links'>
             <li><Link className="nav-link" to="/">Home</Link></li>
-            <li><Link className="nav-link" to="/about">About</Link></li>
-            <li><Link className="nav-link" to="/classes">Classes</Link></li>
             <li><Link className="nav-link" to="/contact">Contact</Link></li>
+            <li><Link className="nav-link" to="/schedule">Schedule</Link></li>
           </ul>
         </nav>
         <div className="login-container">
-          <Link to="/login" className="login-link">Login</Link>
+          <Link to="/login" className="login-link"><img src="/src/assets/icons/login.svg" alt="Login" className="login"/></Link>
         </div>
       </header>
     </>
