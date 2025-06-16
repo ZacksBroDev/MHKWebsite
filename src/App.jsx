145d262b465ad59b50fdac67709a1dff8ddd4484
ZacksBroDev <zackaryzbrown@gmail.com>
2025-06-16 02:59:54 -0400

    added routes to app for added curr pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ import Signup from './pages/signup/Signup';
 import Schedule from './pages/schedule/Schedule';
 import NotFound from './pages/notFoundPage/NotFound';
 import Contact from './pages/contact/contact';
+import Level1 from './pages/fightStyle/level1/Level1';
+import Level2 from './pages/fightStyle/level2/Level2';
+import Level3 from './pages/fightStyle/level3/Level3';
+import Conditional from './pages/fightStyle/blackBelts/conditionals/Conditionals';
+import Deg1 from './pages/fightStyle/blackBelts/deg1/Deg1';
+import Deg2 from './pages/fightStyle/blackBelts/deg2/deg2';
 
 function App() {
   return (
@@ -19,6 +25,13 @@ function App() {
         <Route path="/schedule" element={<Schedule />} />
         <Route path="/contact" element={<Contact/>}/>
         <Route path="*" element={<NotFound />} />
+
+        <Route path="/level1" element={<Level1 />} />
+        <Route path="/level2" element={<Level2 />} />
+        <Route path="/level3" element={<Level3 />} />
+        <Route path="/conditionals" element={<Conditional />} />
+        <Route path="/deg1" element={<Deg1 />} />
+        <Route path="/deg2" element={<Deg2 />} />
       </Routes>
     </>
   )
