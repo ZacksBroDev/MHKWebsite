03368fecf5de155c8827fc8c3457101d13a1673f
ZacksBroDev <zackaryzbrown@gmail.com>
2025-06-16 03:00:17 -0400

    added pages for curr

---
+++ src/pages/fightStyle/level1/Level1.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import '/src/pages/home/home.css'
+
+function Level1() {
+  return (
+    <>
+    <div className="curriculum">
+      <h1>Level 1</h1>
+      <h2>Forms</h2>
+      <div className="level">
+        <h2>fChon Ji</h2>
+        <h3>Link to Video</h3>
+        <ul>
+          <li>1. Step left foot to the left (west) front stance down block, Yell</li>
+          <li>2. Left fist aim, right foot step forward into right front stance, front punch (right hand) 3. Swing right foot into right front stance east, right down block</li>
+          <li>4. Right fist aim, left foot, step forward into left front stance east, front punch (left hand) 5. Shift into left front stand north, left down block</li>
+          <li>6. Left fist aim, right foot, step forward into Right front stance, front punch (right hand) 7. Swing right foot into right front stance South, right down block</li>
+          <li>8. Right fist aim, left foot, step forward into left front stance east, front punch (left hand). Yell 9. Swinging left foot left, shift into right back stance east, left side block.</li>
+          <li>10. Left fist aim, right foot step forward into Right front stance, front punch (right hand) 11. Swing right foot into left back stance west, right side block</li>
+          <li>12. Right fist aim, left foot step forward into Left front stance, front punch (left hand) 13. Swing left foot into right back stance South, left side block</li>
+          <li>14. Left fist aim, Right foot step forward into right front stance, front punch (right hand) 15. Swing right foot into left back stance North, right side block</li>
+          <li>16. Right fist aim, left foot step forward into Left front stance, front punch (left hand) 17. Right foot step into right front stance, right front punch</li>
+          <li>18. Right step back into left front stance, left front punch.</li>
+        </ul>
+        <h2>Kama</h2>
+        <h3>Link to Video</h3>
+        <ul>
+          <li>1. Step left (90*) into left front stance left. Left down block. Right high block. Yell</li>
+          <li>2. Step forward (right foot forward) into right front stance west. Right chop at chest level, left high block.</li>
+          <li>3. Pivot right on left foot into right front stance east. Right down block. Left high block.</li>
+          <li>4. Step forward (left foot forward) into left front stance east. Left chop at chest level, right high block.</li>
+          <li>5. Shift (moving left foot left) into front stance north. Left down block. Right high block.</li>
+          <li>6. Step left foot forward into left front stance north. Right chop chest level. Left high block.</li>
+          <li>7. Pivot to the right on left foot into right front stance south. Right down block. Left high block.</li>
+          <li>8. Step forward (left foot forward) into left front stance south. Left chop at chest level, right high block. Yell</li>
+          <li>9. Shift (left foot to the left) into right back stance east. Square block. Right hand on top</li>
+          <li>10. Step right foot forward into right front stance east. Right chop at chest level left high block</li>
+          <li>11. Pivot right on left foot into left back stance west. Square block. Left hand on top</li>
+          <li>12. Step forward (left foot forward) into left front stance west. Left chop at chest level, right high block.</li>
+          <li>13. Shift (left foot to the left) into right back stance south. Square block. Right hand on top</li>
+          <li>14. Step right foot forward into right front stance south. Right chop at chest level left high block</li>
+          <li>15. Pivot right on left foot into left back stance north. Square block. Left hand on top</li>
+          <li>16. Step forward (left foot forward) into left front stance north. Left chop at chest level, right high block.</li>
+          <li>17. Step forward (right foot forward) into right front stance north. Right chop at chest level, left high block</li>
+          <li>18. Step back (right foot backward) into left front stance north. Left chop at chest level, right high block.</li>
+          <li>19. Left step back into right front stance, right front punch. Yell</li>
+        </ul>
+        <h2>Tan-Gun</h2>
+        <h3>Link to Video</h3>
+        <ul>
+          <li>1</li>
+        </ul>
+        <h2>To-San</h2>
+        <h3>Link to Video</h3>
+        <ul>
+          <li>1</li>
+        </ul>
+        <h2>Won-Hyo</h2>
+        <h3>Link to Video</h3>
+        <ul>
+          <li>1</li>
+        </ul>
+        <h1>Combos</h1>
+        <h2>Karate Combination Set</h2>
+        <h3>Link to Video</h3>
+        <ul>
+          <li>1. Front punch, back punch.</li>
+          <li>2. Front punch, Front kick, back punch.</li>
+          <li>3. Back fist, Step behind Side kick, back punch.</li>
+          <li>4. Front punch, Back leg front kick, Reverse sidekick, back punch.</li>
+          <li>5. Front punch, back punch, Ridge hand.</li>
+          <li>6. Front punch, Front kick, Sidekick, back punch.</li>
+          <li>7. Front punch, Round kick, Side kick, back punch.</li>
+          <li>8. Front Punch, back punch, Back leg round kick, Reverse side kick, back punch.</li>
+        </ul>
+        <h2>American Karate Combination Set</h2>
+        <h3>Link to Video</h3>
+        <ul>
+          <li>1. Back fist, Hook kick, back punch.</li>
+          <li>2. Slide-in Sidekick/ Sidekick, back punch, Back leg round kick, Reverse back fist, Back ridge hand.</li>
+          <li>3. Back fist, Hook kick, Back leg round kick, Reverse side kick, back punch.</li>
+          <li>4. Front punch, back punch, Back leg front kick (low)/round kick (high), Reverse sidekick, Back ridge hand.</li>
+          <li>5. Palm, Fade, Switch palm.</li>
+          <li>6. Jam/jam, Palm, Switch round kick cover, Switch palm, Grab, Back leg round kick.</li>
+          <li>7. Palm, Fade, Switch round kick cover, Step forward jam, Palm.</li>
+          <li>8. Back hand parry, Front hand parry, Palm, Switch round kick cover/round kick cover, Step forward jam, Palm.</li>
+        </ul>
+      </div>
+      <Link to="/"><button>Home</button></Link>
+    </div>
+  </>
+  );
+}
+export default Level1;
---
+++ src/pages/fightStyle/level2/Level2.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Level2() {
+  return (
+    <>
+      <div className="curriculum">
+        <h1>Level 2</h1>
+        <h2>Forms</h2>
+        <div className="level">
+          <h2>fChon Ji</h2>
+          <h3>Link to Video</h3>
+          <ul>
+            <li>1</li>
+          </ul>
+          <h2>Kama</h2>
+          <h3>Link to Video</h3>
+          <ul>
+            <li>1</li>
+          </ul>
+          <h2>Tan-Gun</h2>
+          <h3>Link to Video</h3>
+          <ul>
+            <li>1</li>
+          </ul>
+          <h2>To-San</h2>
+          <h3>Link to Video</h3>
+          <ul>
+            <li>1</li>
+          </ul>
+          <h2>Won-Hyo</h2>
+          <h3>Link to Video</h3>
+          <ul>
+            <li>1</li>
+          </ul>
+          <h1>Combos</h1>
+          <h2>Tae Kwon Do Combination Set</h2>
+          <h3>Link to Video</h3>
+          <ul>
+            <li>1. Front punch, Inside-outside kick, back punch.</li>
+            <li>2. Front punch, Lunge punch, Reverse hook kick, back punch.</li>
+            <li>3. Slide-in round kick high/low, Overhand back fist, Drop back punch.</li>
+            <li>4. Defensive side kick, Step behind Side kick, Back leg round kick, Back hand back fist, Back leg round kick.</li>
+            <li>5. Defensive side kick/ Slide-in hook kick/round kick, Overhand back fist, Mid-section back punch.</li>
+            <li>6. Jump round kick, Reverse back fist, Back leg round kick, back punch.</li>
+            <li>7. Slide-in round kick/side kick, Reverse side kick, Back leg round kick, back punch.</li>
+            <li>8. Slide-in hook kick, Back leg round kick, Reverse back fist, Back ridge hand.</li>
+          </ul>
+          <h2>American Tae Kwon Do Combination Set</h2>
+          <h3>Link to Video</h3>
+          <ul>
+            <li>1. Jump back leg round kick, Reverse hook kick, back punch.</li>
+            <li>2. Jump back leg side kick, Reverse side kick, back punch.</li>
+            <li>3. Front punch, Front kick, Back leg round kick, reverse hook/round kick, back punch.</li>
+            <li>4. Front punch/ridge hand, Reverse back fist, Back leg round kick, Back fist, back punch.</li>
+            <li>5. Lateral ridge hand, Hook kick, Back fist, Ridge hand.</li>
+            <li>6. Lateral round kick, Back fist, back punch, Ridge hand, Round kick.</li>
+            <li>7. Fade ridge hand, Hook kick, back punch.</li>
+            <li>8. Fade side kick, Hook kick/round kick, Front punch, back punch.</li>
+          </ul>
+        </div>
+        <Link to="/"><button>Home</button></Link>
+      </div>
+    </>
+  );
+}
+export default Level2;
