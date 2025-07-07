4d1b1e6efd1ad14e891ca37713b80482165b37a5
Zack Brown <zackaryzbrown@gmail.com>
2025-07-06 20:01:09 -0600

    level added content

--- src/pages/fightStyle/level2/Level2.jsx
+++ src/pages/fightStyle/level2/Level2.jsx
@@ -6,42 +6,167 @@ function Level2() {
   return (
     <>
       <div className={css.curriculum}>
-        <h1>Level 2</h1>
+        <h1>Level 2 Hyung</h1>
         <h2>Forms</h2>
         <div className={css.level}>
           <h2>Yuk Kuk</h2>
           <h3><a href="https://vimeo.com/844708861?share=copy">Link to Video</a></h3>
+          <p>Ready Stance</p>
           <ul>
-            
-            <li>1</li>
+            <li>1. Move left foot into horse stance north. Right hand aim.</li>
+            <li>2/3. Slow (2-3 sec) left punch. Right punch left punch</li>
+            <li>4. Skip right into horse stance. Slow (2-3 sec) right punch</li>
+            <li>5/6. left punch right punch</li>
+            <li>7. Step into right front stance northeast (or 45-degree angle) Right side block</li>
+            <li>8. Back leg front kick (left leg)</li>
+            <li>9/10. As land, front punch back punch</li>
+            <li>11. Shift into left front stance northwest (or 45-degree) left side block</li>
+            <li>12. back leg front kick (right leg)</li>
+            <li>13/14. Front punch back punch</li>
+            <li>15. shift right into north front stance, slow right chop (2-3 sec)</li>
+            <li>16. slow left chop (2-3 sec)</li>
+            <li>17. back (right) punch</li>
+            <li>18. step (left leg) forward into left front stance; Slow left chop (2-3 sec)</li>
+            <li>19. Slow right chop (2-3 sec)</li>
+            <li>20. Back punch (left)</li>
+            <li>21. Step (right leg) forward into right front stance. punch. Yell</li>
+            <li>22. Feet together (bring left to right-facing back door (east))</li>
+            <li>23. Left leg side kick</li>
+            <li>24. While landing in left front stance (north) right elbow strike into left palm</li>
+            <li>25. Feet together east (right to left)</li>
+            <li>26. Right side kick</li>
+            <li>27. While landing in right front stance (south) left elbow strike into right palm</li>
+            <li>28. Shift into right back stance (moving left foot) east. Open hand square block</li>
+            <li>29. Step into right front stance east. Right spear finger.</li>
+            <li>30. Shift clockwise (to right) into left back stance. Open square hand block</li>
+            <li>31. Step into left front stance west. Left hand spear finger.</li>
+            <li>32/33. Shift (left/counter clockwise) into left front stance south. Left outer block. Right punch</li>
+            <li>34/35. Step forward into right front stance south. Right outer block. Left punch</li>
+            <li>36. Move into left cross stance, right foot behind. Left leg south, left back fist. Yell</li>
+            <li>37. Shift (pivot) right (clockwise) into right front stance east, double forearm block</li>
+            <li>38. “skip” (right foot to left foot, then left foot) into left front stance west, double forearm block. Yell</li>
+            <li>End: Bring the left foot back to a ready position.</li>
           </ul>
         </div>
         <div className={css.level}>
-        <h2>Chun Gun</h2>
+        <h2>Chun Gun (Joong Gun)</h2>
         <h3><a href="https://vimeo.com/838875858?share=copy">Link to Video</a></h3>
+        <p>Ready Posture: Closed Ready Stance (Feet together, left hand grabbing right fist below sternum)</p>
         <ul>
-          <li>1</li>
+          <li>1. Step into right back-stance west, left open-hand side block (or Left Middle Ridgehand Block), yell.</li>
+          <li>2. Left leg Front kick</li>
+          <li>3. land with left foot in front, Step into right foot in front Cat Stance, Right Upward Palm Heel Block (Shoulder level)</li>
+          <li>4. Pivot right on Left Foot into Left back-stance East, right open-hand side block (or right Middle Ridgehand Block)</li>
+          <li>5. Right leg Front kick</li>
+          <li>6. land with right foot in front, Step into left foot in front Cat Stance, left Upward Palm Heel Block (Shoulder level)</li>
+          <li>7. Shift to the left to face north into a Back Stance Sudo (left foot forward)</li>
+          <li>8. Shift into left foot front stance executing a Right Upper Elbow Strike (answer the phone)</li>
+          <li>9. Step into left back-stance sudo</li>
+          <li>10. Shift into right foot front stance executing a Left Upper Elbow Strike (answer the phone)</li>
+          <li>11. Step into left front stance while performing double punch: face level</li>
+          <li>12. Step into right foot front stance while executing a Twin Fist Uppercut (at about upper chest level) - YELL.</li>
+          <li>13. Shift into left front-stance south (turning to the left, moving only left foot) high closed hand x-block</li>
+          <li>14. Shift into right back-stance (facing east) left back fist</li>
+          <li>15/16. Shift into left front stance East, left inside block, right punch</li>
+          <li>17. Step Right into feet together facing south, then right into left back-stance facing west, right back fist.</li>
+          <li>18/19. Shift into Right front stance West, right inside block, left punch</li>
+          <li>20. Step feet together, right to left. (Bring the right foot to the left foot) double fist bump chamber. Step Left foot toward South (left) into a Left Front Stance south. Left Double Forearm High Block.</li>
+          <li>21. Shift into right back-stance south, left punch</li>
+          <li>22/23. Back leg side kick (right leg), (after kick, while chamber, chamber hands in double fist bump at left shoulder) land in right front stance, right double forearm block</li>
+          <li>24. Shift right leg into left back stance south while aim with left fist then right front punch</li>
+          <li>25/26. back leg (left) side kick still facing south, land in right back stance (left foot forward) with a closed hand sudo.</li>
+          <li>27. Shift into left front stance south, left hand low right hand high slow (2-3 sec) press block</li>
+          <li>28. Step into left back-stance, closed hand sudo</li>
+          <li>29. Shift into right front stance south, left hand low right hand high slow (2-3 sec) press block</li>
+          <li>30. Step feet together, left foot to right foot, shift to face east (to the left), same position as cross body punch (left hand chamber at ribs, right fist at left shoulder)</li>
+          <li>31. Step into left back-stance East, stick block (left hand high, right hand low - about chest level)</li>
+          <li>32. Skip (bring right foot to left foot, then left foot step) into right back-stance West, stick block. Yell.</li>
+          <li>End: Bring the left foot back to the ready position, feet together left hand grabbing right fist below sternum</li>
         </ul>
         </div>
         <div className={css.level}>
-          <h2>Toi Gye</h2>
+          <h2>Toi Gye Hyung</h2>
           <h3><a href="https://vimeo.com/844706053?share=copy">Link to Video</a></h3>
-          <ul>
-            <li>1</li>
+          <p>Ready stance Feet together. Left hand grabbing right fist at solar plexus (like around sternum)</p>
+          <ul> 
+            <li>1. Step left foot left into right back stance (west). Left side block. Yell</li>
+            <li>2. Shift into left front stance west. Left palm heel block across body at shoulder level, right spear hand to groin, palm up.</li>
+            <li>3. Feet together north, left foot to right foot. Cross left arm on top. Left down block, right outer block.</li>
+            <li>4. Step right into left back stance east. Right side block (left chamber at ribs)</li>
+            <li>5. Shift into right front stance east. Right palm heel block across body at shoulder level, left spear hand to groin, palm up.</li>
+            <li>6. Turn left feet together north, right to left. Cross right arm on top. Right down block, left outer block.</li>
+            <li>7/8. Step forward into left front stance north. Low closed hand X block. Double punch face level. Yell</li>
+            <li>9/10/11. Maintain hand position, back leg front kick(9) land right front stance north. Right front punch(10) left front punch (11)</li>
+            <li>12. Slowly feet together west, left foot to right foot. Hands move from a mountain block to fists on hip (Superman move)</li>
+            <li>13. Pivot left on left foot (move right foot) into horse stance south. Wide mountain block.</li>
+            <li>14. Pivot left on left foot (moving right foot to the left) into horse stance north. Wide mountain block.</li>
+            <li>15. Step feet together, right foot to left foot. Step into right back stance north. Low double forearm block</li>
+            <li>16/17. Shift into left front stance north. Reach and grab head triangle to right knee strike. Yell.</li>
+            <li>18. Land feet together west, step into right back stance south.</li>
+            <li>19/20. Maintain hand position, left front kick. Aim with right open hand palm down at eye level. Land left front stance south, left spear hand.</li>
+            <li>21. Step forward into left back stance south.</li>
+            <li>22. Maintain hand position. Right leg front kick. Aim with left open hand, palm down at eye level. Land right front stance south. Right spear hand.</li>
+            <li>23. Step back into right back stance south. Left down block south and high right hand back fist surfer.</li>
+            <li>24. Jump forward (south), land right cross stance east, but head facing south. Low closed hand X block east. Yell</li>
+            <li>25. Step into right front stance south. Double forearm block.</li>
+            <li>26. Pivot left on right foot into right back stance west. Low side block.</li>
+            <li>27. Shift into left front stance west. Right side block NW (45 degree angle)</li>
+            <li>28. Step feet together, step into left back stance east. Low side block.</li>
+            <li>29. Shift into right front stance east. Left side block NE (45 degree angle)</li>
+            <li>30. Pivot into left front stance north. Right side block NE (45 degree angle)</li>
+            <li>31. Pivot into right front stance east, left side block NE (45 degree angle)</li>
+            <li>32. Step into horse stance north. Right punch. Yell</li>
           </ul>
           </div>
         <div className={css.level}>
-          <h2>Hwa Rang</h2>
+          <h2>Hwa Rang Hyung</h2>
           <h3><a href="https://vimeo.com/844710112?share=copy">Link to Video</a></h3>
+          <p>Ready stance: Feet together (north), hands waist level, palms open, left hand on top</p>  
           <ul>
-            <li>1</li>
+            <li>1. Move left foot into horse stance, left open hand palm block at a 45 degree angle</li>
+            <li>2/3. Right punch, left punch</li>
+            <li>4. Shift right foot right into left back stance, closed hand square block</li>
+            <li>5. left hammer fist, palm up and right hand inner block, ending at right shoulder</li>
+            <li>6. Shift right foot forward into right fixed stance, right punch</li>
+            <li>7. Bring right foot to left, facing north (mirrors), right hand rainbow</li>
+            <li>8. Step the left foot east (toward back doors) into left front stance, left punch</li>
+            <li>9. Shift left foot to left (toward mirrors) into left front stance. Left down block</li>
+            <li>10. Step forward into right front stance, right punch</li>
+            <li>11. using hands to “pull” bring left foot to right foot, body facing north, feet facing west (front doors) set for side kick</li>
+            <li>12. Right side kick. After kick, chamber, then step into left back stance, right hand chop</li>
+            <li>13. Step left foot forward into left front stance, left punch</li>
+            <li>14. ddddkjbsadliuhgsdiuhsiugsfd ?????</li>
+            <li>15. Step the right foot toward D into a Right Walking Stance facing D, at the same time executing a Right Middle Punch to D.</li>
+            <li>16. Step the left foot toward E, turning counterclockwise to form a Right L-Stance (i.e., left foot forward) facing E. Middle Knifehand Guarding Block to E.</li>
+            <li>17. Step the right foot toward E into a Right Walking Stance facing E. Right Vertical Fingertip Thrust (spearhand) to E.</li>
+            <li>18. Move the right foot on line EF into a Right L-Stance (i.e., left foot forward) facing F. Middle Knifehand Guarding Block to F.</li>
+            <li>19. Perform 18 and 19 in a fast motion:</li>
+            <li>18. Execute a Right High Turning Kick to DF and then lower the right foot to F.</li>
+            <li>19. Execute a Left High Turning Kick to CF and then lower the left foot to F, into a Right L-Stance (i.e., left foot forward) facing F. Middle Knifehand Guarding Block to F.</li>
+            <li>20. Step the left foot toward C into a Left Walking Stance facing C. Left Low Block to C.</li>
+            <li>21. Execute a Right Middle Punch to C, pulling the right foot into a Right L-Stance (i.e., left foot forward) facing C.</li>
+            <li>22. Step the right foot toward C into a Left L-Stance (i.e., right foot forward) facing C. Left Middle Punch to C.</li>
+            <li>23. Step the left foot toward C into a Right L-Stance (i.e., left foot forward) facing C. Right Middle Punch to C.</li>
+            <li>24. Execute a X-Fist Pressing Block, pulling the left foot into a Left Walking Stance facing C, to C.</li>
+            <li>25. Step the right foot toward C in a, sliding motion forming a Right L-Stance (i.e., left foot forward) facing D. Right Elbow Thrust to C.</li>
+            <li>26. Bring the left foot to the right foot, turning counterclockwise to form a Closed Stance facing B. Right Inner Forearm Side Front Block, while extending the left forearm downard to the side.</li>
+            <li>27. Execute a Left Inner Forearm Side Front Block, extending the right forearm downward to the side, while maintaining a Closed Stance facing B.</li>
+            <li>28. Step the left foot toward B into a Right L-Stance (i.e., left foot forward) facing B at the same time executing a Middle Knifehand Guarding Block to B.</li>
+            <li>29. Bring the left foot to the right foot and then step the right foot toward A into a Left L-Stance (i.e., right foot forward) facing A. Middle Knifehand Guarding Block to A.</li>
           </ul>
         </div>
         <div className={css.level}>
           <h2>Kama 2</h2>
           <h3><a href="https://vimeo.com/853554085?share=copy">Link to Video</a></h3>
+          <p>Ready position</p>
           <ul>
-            <li>1</li>
+            <li>1. Feet together, kamas chest level crossed. Left hand in front.</li>
+            <li>2. Move left foot left into horse stance north. Left hand inside chop. Down block. Right hand inside chop. Down block. Yell</li>
+            <li>3./4. Shift into left back stance east. Square block, left high block right outside block left hand inside chop. Down block. Move right foot into fixed stance. Right hand inside chop, down block.</li>
+            <li>5. Step forward into left front stance east, left hand inside chop, down block. Right inside chop, down block.</li>
+            <li>6. Shift (left foot left) into left front stance north. Left down block, right high block.</li>
+            <li>7. Step forward into right front stance north. Right chop at chest level, left high block. Chamber right hand on top left hand at hip. Step feet together left foot to right foot. Right hand chop.</li>
+            <li>8. </li>
           </ul>
         </div>
         <h2>Combos</h2>
